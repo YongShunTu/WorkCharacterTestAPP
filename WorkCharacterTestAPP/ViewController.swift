25b6f829fc0354a0752a343c4af9fe405000b70a
YongShunTu <a49980060@gmail.com>
2021-12-19 02:48:59 +0800

    Initial Commit

---
+++ WorkCharacterTestAPP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WorkCharacterTestAPP
+//
+//  Created by 姜又寧 on 2021/12/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
